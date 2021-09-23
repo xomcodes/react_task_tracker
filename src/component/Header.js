097fe516b2a01a626ca86169e1580591cc289135
Chisom Okeoma <xommyb@gmail.com>
2021-09-23 09:59:35 +0100

    initial commit

---
+++ src/component/Header.js
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+import Button from './Button';
+
+const Header = ({title, onAdd, showAdd}) => {
+    
+    return (
+        <header className ='header'>
+            <h1>{title}</h1>
+         <Button color = {showAdd ? 'red' : 'green'} text ={showAdd ? 'Close' : 'Add'} onClick = {onAdd}/>
+         
+        </header>
+    )
+}
+
+// Css in Js
+// const headingStyle ={
+//     color:'purple',
+//     backgroundColor: 'black',
+// }
+
+
+
+Header.defaultProps = {
+    title: 'Task Tracker'
+}
+
+Header.propTypes = {
+   title: PropTypes.string.isRequired, 
+}
+
+
+export default Header
